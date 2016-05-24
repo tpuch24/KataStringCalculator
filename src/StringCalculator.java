94036fe007c4b1f76733160186bb295ab274a611
Thierry Puch <t.puch@laposte.net>
2016-05-24 11:34:53 +0200

    After test for one number

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,11 +2,16 @@
 public class StringCalculator {
 
 	int add(String str){
+		int intSum=0;
 		
 		if(str.length()==0){
-			System.out.println("Chaine vide");
+			//System.out.println("Chaine vide");
 			return 0;
 		}
-		return 0;
+	      for (String retval: str.split(",")){
+	          //System.out.println(retval);
+	    	  intSum+=Integer.parseInt(retval);
+	       }
+		return intSum;
 	}
 }
