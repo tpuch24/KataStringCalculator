62dab130980e7a9aa6acbe5d574ddc8a36c47b22
xpros64 <xpros64@hotmail.fr>
2016-05-24 09:44:31 +0200

    Test 1 and began calculator method

--- src/StingCalculatorTest.java
+++ src/StingCalculatorTest.java
@@ -1,4 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class StingCalculatorTest {
+	
+	@Test
+	public void emptyStringTest()
+	{
+		//given
+		StringCalculator sc = new StringCalculator();
+		//when
+		String result = sc.calculator("");
+		//then
+		assertEquals(result,"");
+	}
+	
 
 }
--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,4 +1,12 @@
 
 public class StringCalculator {
 
+	String calculator(String str){
+		
+		if(str.length()==0){
+			System.out.println("Chaine vide");
+			return "";
+		}
+		return str;
+	}
 }
